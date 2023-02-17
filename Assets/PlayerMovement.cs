16f96ac047872d3859a1ae728526a35637ed4e65
Alon Shakaroff <alon_lect@hotmail.com>
2023-02-17 13:39:34 +0200

    Sideway movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,20 +7,28 @@ using UnityEngine.UI;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]float m_JumpPower = 14f;
-    private Rigidbody2D m_PlayerRigidBody;
+    [SerializeField]float m_SideMovementPower = 7f;
+
+    private Rigidbody2D m_Rb;
 
     // Start is called before the first frame update
     private void Start()
     {
-       m_PlayerRigidBody = GetComponent<Rigidbody2D>();
+       m_Rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     private void Update()
     {
+        //  if you want a smooth stop and not a hard stop then change it to 
+        //  GetAxis instead of GetAxis raw, so it changes the axis gradually.
+        float directionX = Input.GetAxisRaw("Horizontal");
+        
+        m_Rb.velocity = new Vector2(directionX * m_SideMovementPower, m_Rb.velocity.y);
+
         if (Input.GetButtonDown("Jump"))
         {
-           m_PlayerRigidBody.velocity = new Vector3(0, m_JumpPower, 0);
+           m_Rb.velocity = new Vector2(m_Rb.velocity.x, m_JumpPower);
         }
     }
 }

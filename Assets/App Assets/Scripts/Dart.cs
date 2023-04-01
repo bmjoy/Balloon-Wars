cd89d6ff4209ffaa85222d8d9c6f1845de3b65de
Alon Shakaroff <alon_lect@hotmail.com>
2023-04-01 22:21:18 +0300

    Projection point for aiming darts

--- Assets/App Assets/Scripts/Dart.cs
+++ Assets/App Assets/Scripts/Dart.cs
@@ -4,17 +4,36 @@ using UnityEngine;
 
 public class Dart : MonoBehaviour
 {
-    Rigidbody2D m_RigidBody;
+    private Rigidbody2D m_RigidBody;
+    private bool m_HasHit = false;
+    [SerializeField] private float m_DartDestroyTime = 3f;
 
     private void Start()
     {
         m_RigidBody = GetComponent<Rigidbody2D>();
-        //transform.Rotate(0, 0, -90);
     }
 
     private void Update()
     {
-        float angle = Mathf.Atan2(m_RigidBody.velocity.y, m_RigidBody.velocity.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        if (!m_HasHit)
+        {
+            float angle = Mathf.Atan2(m_RigidBody.velocity.y, m_RigidBody.velocity.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) 
+    {
+        m_HasHit = true;
+        m_RigidBody.velocity = Vector2.zero;
+        m_RigidBody.isKinematic = true;
+        StartCoroutine(DestroyDart());
+    }
+
+    private IEnumerator DestroyDart()
+    {
+        yield return new WaitForSeconds(m_DartDestroyTime);
+        Destroy(gameObject);
     }
 }
+

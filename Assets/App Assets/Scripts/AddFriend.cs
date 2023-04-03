ae2349d706e0095e4ce85ef0b6da83f90d958593
Omer Cohen Shor <omercs8@gmail.com>
2023-04-03 05:40:39 +0300

    friendlist UI

---
+++ Assets/App Assets/Scripts/AddFriend.cs
@@ -0,0 +1,22 @@
+using System
+using UnityEngine;
+
+public class AddFriend : MonoBehaviour
+{
+    [SerializeField] private string displayName;
+
+    public static Action<string> OnAddFrieand;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

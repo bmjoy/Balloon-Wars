d5a9d4eb0f64730fde963928738614b78bf3912c
Omer Cohen Shor <omercs8@gmail.com>
2023-05-11 21:18:28 +0300

    Update PowerUpWrapper.cs

--- Assets/App Assets/Scripts/Game scripts/PowerUp Scripts/PowerUpWrapper.cs
+++ Assets/App Assets/Scripts/Game scripts/PowerUp Scripts/PowerUpWrapper.cs
@@ -1,11 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PowerUpWrapper : MonoBehaviour
 {
     [SerializeField] List<GameObject> m_PowerUpPrefabs;
     PowerUp m_CurPowerUp;
+    private void generateNewPowerUp()
+    {
+
+    }
+    
+    private void destroyOldPowerUp()
+    {
+
+    }
+
+    private void ActivatePowerUp(GameObject player, int PowerUpTime)
+    {
+        StartCoroutine(StartPowerUpForTime(player, PowerUpTime));
+    }
+
+    private IEnumerator StartPowerUpForTime(GameObject player, int time)
+    {
+        m_CurPowerUp.activatePowerUp(player);
+        yield return new WaitForSeconds(time);
+        m_CurPowerUp.deActivatePowerUp(player);
+    }
+
+
     void Start()
     {
         

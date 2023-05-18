4f3719bc5a986e3a5003ab8c0687de1fa63d801d
Omer Cohen Shor <omercs8@gmail.com>
2023-05-18 09:49:19 +0300

    Import characters asset

---
+++ Assets/PawerPack/Scripts/Manager.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace PawerPack_All
+{
+
+    public class Manager : MonoBehaviour
+    {
+
+        public int counter = 0;
+        public float delay = 1f;
+        public Animator[] Animators;
+        // Use this for initialization
+        void Start()
+        {
+            StartCoroutine(ChangeAnimation());
+        }
+
+        IEnumerator ChangeAnimation()
+        {
+
+            yield return new WaitForSeconds(delay);
+
+            int maxAnimation = 2;
+            counter++;
+            if (counter > maxAnimation)
+            {
+                counter = 1;
+            }
+
+
+            if(counter==1)
+            {
+                foreach (Animator an in Animators)
+                {
+                    an.Play("Walk");
+                    delay = 3;
+
+
+                }
+            }
+            else if (counter == 2)
+            {
+                foreach (Animator an in Animators)
+                {
+                    an.Play("Idle");
+                    delay =.5f;
+
+                }
+            }
+
+            StartCoroutine(ChangeAnimation());
+
+        }
+
+
+     }
+
+
+
+
+     
+}
+
